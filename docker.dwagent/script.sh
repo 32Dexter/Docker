21bb6a58d7695b15d8117a1f7009dba0938d7a31
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-05-17 17:57:15 +0200

    Update script.sh

--- docker.dwagent/script.sh
+++ docker.dwagent/script.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-printf '1\n/datacenter/dwagent\n1\n1\n'$CODE | ./home/dwagent.sh
+string1=$(echo $CODE | wc -l)
+
+if [[ $string1 -gt 0 ]]; then
+      printf '1\n/datacenter/dwagent\n1\n1\n'$CODE | ./home/dwagent.sh
+else
+     exit(0)
+fi
